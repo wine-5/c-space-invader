62dd7a116879581e2e4855bc706841bcd560fbf5
YutoImata <yuto.imata.wine@gmail.com>
2026-07-24 21:20:14 +0900

    feat: とりあえずDxLibでビルドできるように

--- Invader/src/Main.c
+++
@@ -1,7 +0,0 @@
-﻿#include <stdio.h>
-
-int main()
-{
-	int num = 10;
-	printf("%d", num);
-}
---
+++ Invader/src/Main.cpp
@@ -0,0 +1,29 @@
+﻿#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	// 画面モードのセット
+	SetGraphMode(640, 480, 16, 60);
+	if (DxLib_Init() == -1)	// ＤＸライブラリ初期化処理
+	{
+		return -1;				// エラーが起きたら直ちに終了
+	}
+
+	// 描画先画面を裏画面にセット
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	// ループ
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		// 画面を初期化する
+		//ClearDrawScreen();
+
+		// 裏画面の内容を表画面に反映させる
+		ScreenFlip();
+	}
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;					// ソフトの終了
+}
